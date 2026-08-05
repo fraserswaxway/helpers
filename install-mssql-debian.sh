17954df1564cf28ecf7834a9d5f231492e1429c1
fraserswaxway <60369409+fraserswaxway@users.noreply.github.com>
2026-08-05 21:06:16 +0000

    path corrections

--- install-mssql-debian.sh
+++ install-mssql-debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+# curl -L https://raw.githubusercontent.com/fraserswaxway/helpers/refs/heads/main/install-mssql-debian.sh | bash
 rm -rf /opt/sqlserver
-mkdir -p /opt/sqlserver
+mkdir -p /opt/sqlserver/var/opt/mssql
 chmod -R 777 /opt/sqlserver
 
 docker run \
@@ -10,5 +10,5 @@ docker run \
   -p 1433:1433 \
   --name SQLserver \
   --mount type=bind,source=/tmp,target=/tmp/host \
-  --mount type=bind,source=/axwaynfs/docker/mount/sqlserver/var/opt/mssql,target=/var/opt/mssql \
+  --mount type=bind,source=/opt/sqlserver/var/opt/mssql,target=/var/opt/mssql \
   -d mcr.microsoft.com/mssql/server:2019-latest
